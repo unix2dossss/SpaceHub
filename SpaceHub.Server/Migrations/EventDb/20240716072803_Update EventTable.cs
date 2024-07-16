a457c7b5d2460b2afb8bfc87ba86929e1139be40
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-07-16 22:43:14 +1200

    Create Event Date Function

---
+++ SpaceHub.Server/Migrations/EventDb/20240716072803_Update EventTable.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpaceHub.Server.Migrations.EventDb
+{
+    /// <inheritdoc />
+    public partial class UpdateEventTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EventPast",
+                table: "Events");
+
+            migrationBuilder.RenameColumn(
+                name: "EventTime",
+                table: "Events",
+                newName: "EventEndTime");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EventDateTime",
+                table: "Events",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EventDateTime",
+                table: "Events");
+
+            migrationBuilder.RenameColumn(
+                name: "EventEndTime",
+                table: "Events",
+                newName: "EventTime");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "EventPast",
+                table: "Events",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
--- SpaceHub.Server/Migrations/EventDb/20240716073731_Add DateTime EventDateTime & EventEndTime.Designer.cs
+++ SpaceHub.Server/Migrations/EventDb/20240716073731_Add DateTime EventDateTime & EventEndTime.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SpaceHub.Server.Data;
 
@@ -11,9 +12,11 @@ using SpaceHub.Server.Data;
 namespace SpaceHub.Server.Migrations.EventDb
 {
     [DbContext(typeof(EventDbContext))]
-    partial class EventDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240716073731_Add DateTime EventDateTime & EventEndTime")]
+    partial class AddDateTimeEventDateTimeEventEndTime
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -24,11 +27,8 @@ namespace SpaceHub.Server.Migrations.EventDb
 
             modelBuilder.Entity("SpaceHub.Server.Models.Event", b =>
                 {
-                    b.Property<int>("EventId")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("EventId"));
+                    b.Property<string>("EventName")
+                        .HasColumnType("nvarchar(450)");
 
                     b.Property<string>("EventCardBG")
                         .IsRequired()
@@ -52,11 +52,7 @@ namespace SpaceHub.Server.Migrations.EventDb
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("EventName")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.HasKey("EventId");
+                    b.HasKey("EventName");
 
                     b.ToTable("Events");
                 });
