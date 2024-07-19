262dd442678f91d7b388b6602176a335a63720db
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-07-19 16:05:25 +1200

    Smalle UI Updates

--- spacehub.client/src/Components/Homepage/Home.tsx
+++ spacehub.client/src/Components/Homepage/Home.tsx
@@ -1,9 +1,11 @@
 import Hero from './Hero';
+import Hero2 from './Hero2';
 
 function Home(){
     return (
         <div>
-            <Hero></Hero>
+            {/*<Hero></Hero>*/}
+            <Hero2></Hero2>
         </div>
     );
 }
